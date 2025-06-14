7fd7ab6afdc690df4a0987b5f46f6ec2ffd5979e
ramong26 <rlatndus21049@gmail.com>
2025-06-14 18:21:43 +0900

    D

--- src/features/home/types/productType.ts
+++ src/features/home/types/productType.ts
@@ -1,17 +1,6 @@
 export interface Product {
   nextCursor: number;
-  list: {
-    id: number;
-    name: string;
-    image: string;
-    rating: number;
-    reviewCount: number;
-    favoriteCount: number;
-    categoryId: number;
-    writerId: number;
-    createdAt: string;
-    updatedAt: string;
-  };
+  list: ProductList[];
 }
 
 export interface ProductList {
