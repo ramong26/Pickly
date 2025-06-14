dee05c0f6b8042a63c281673940b240769f36e6f
ramong26 <rlatndus21049@gmail.com>
2025-06-14 18:57:16 +0900

    d

--- src/lib/axios/index.ts
+++ src/lib/axios/index.ts
@@ -1,16 +1,9 @@
+// lib/axios/index.ts
 import axios from "axios";
 
-
-// const token = localStorage.getItem("accessToken");
-
 const apiInstance = axios.create({
-  
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-
-  //   withCredentials: true,
-  //   headers: {
-  //     Authorization: `Bearer ${token}`,
-  //   },
-
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL, // ← 이 값이 undefined라면 에러 발생
+  timeout: 5000,
 });
+
 export default apiInstance;
