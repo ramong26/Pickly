44e91442643cbdb95ca6c854c10ce5e7b24fbed7
ramong26 <rlatndus21049@gmail.com>
2025-06-14 19:13:34 +0900

    D

---
+++ src/lib/axios.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const apiInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL, // API 서버 기본 주소
+  headers: {
+    "Content-Type": "application/json",
+  },
+  // 필요에 따라 타임아웃, 인터셉터 등 설정 가능
+});
+
+export default apiInstance;
