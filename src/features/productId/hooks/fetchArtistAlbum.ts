dbeeeb33671796fdc4f5af72cfd5b1ddf214a1f6
ramong26 <rlatndus21049@gmail.com>
2025-06-16 00:52:06 +0900

    d

--- src/features/productId/hooks/fetchArtistAlbum.ts
+++ src/features/productId/hooks/fetchArtistAlbum.ts
@@ -1,19 +1,7 @@
 // ai에게 post 요청을 보내 아티스트와 앨범 이름을 추출하는 함수
 
 export default async function fetchArtistAlbum(text: string) {
-  const baseUrl =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
-      : process.env.NEXT_PUBLIC_BASE_URL || "https://mogazoa-api.vercel.app";
-
-  const apiPath = "/api/14-6/openai";
-
-  const normalizedBaseUrl = baseUrl.endsWith("/")
-    ? baseUrl.slice(0, -1)
-    : baseUrl;
-
-  const url = normalizedBaseUrl + apiPath;
-
+  const url = "/api/14-6/openai";
   const res = await fetch(url, {
     method: "POST",
     headers: {
