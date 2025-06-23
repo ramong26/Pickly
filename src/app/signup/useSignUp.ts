52aa20191e1c818e3826181efff6d4ef051128f9
ramong26 <rlatndus21049@gmail.com>
2025-06-23 12:27:37 +0900

    D

--- src/app/signup/useSignUp.ts
+++ src/app/signup/useSignUp.ts
@@ -1,53 +1,21 @@
 import apiInstance from "@/lib/axios";
 import { useMutation, UseMutationOptions } from "@tanstack/react-query";
-
+import { JoinForm } from "./validationSchema";
 import { AuthResponse } from "../signin/validationSchema";
 import { AxiosError } from "axios";
 
-interface OAuthSignUpForm {
-  redirectUri: string;
-  token: string;
-  provider: "google" | "kakao";
-  nickname: string;
-}
-
-export function useOAuthSignUpMutation(
-  options?: UseMutationOptions<AuthResponse, AxiosError, OAuthSignUpForm>
+export function useSignUp(
+  options?: UseMutationOptions<AuthResponse, AxiosError, JoinForm>
 ) {
   const mutation = useMutation({
-    mutationFn: async ({
-      redirectUri,
-      token,
-      provider,
-      nickname,
-    }: OAuthSignUpForm) => {
-      // 1. 백엔드에 OAuth 회원가입 요청
-      const res = await apiInstance.post<AuthResponse>(
-        `/auth/signUp/${provider}`,
-        {
-          redirectUri,
-          token,
-          nickname,
-        }
-      );
-
-      const data = res.data;
-
-      // 2. 받은 accessToken을 Next.js API Route로 전달하여 쿠키 저장
-      await fetch("/api/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          accessToken: data.accessToken,
-        }),
-      });
-
-      return data;
+    mutationFn: async (form: JoinForm) => {
+      const res = await apiInstance.post<AuthResponse>("/auth/signUp", form);
+      return res.data;
     },
     ...options,
   });
 
-  return mutation;
+  return {
+    ...mutation,
+  };
 }
